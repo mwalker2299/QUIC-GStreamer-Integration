f6fc06a154452b1f66a6f25f0f1e2acb6812992e
mjwalker2299 <mjwalker2299@gmail.com>
2021-10-14 12:33:56 +0100

    Removed unnecessary function overrides from gstquicsrc

--- src/gst-quic/plugins/gstquicsrc.c
+++ src/gst-quic/plugins/gstquicsrc.c
@@ -49,6 +49,8 @@ GST_STATIC_PAD_TEMPLATE ("src",
     GST_PAD_ALWAYS,
     GST_STATIC_CAPS_ANY);
 
+/* property templates */
+
 enum
 {
   PROP_0
@@ -68,32 +70,19 @@ static void gst_quicsrc_set_property (GObject * object,
     guint property_id, const GValue * value, GParamSpec * pspec);
 static void gst_quicsrc_get_property (GObject * object,
     guint property_id, GValue * value, GParamSpec * pspec);
+
 static void gst_quicsrc_dispose (GObject * object);
 static void gst_quicsrc_finalize (GObject * object);
 
 static GstCaps *gst_quicsrc_get_caps (GstBaseSrc * src, GstCaps * filter);
-static gboolean gst_quicsrc_negotiate (GstBaseSrc * src);
-static GstCaps *gst_quicsrc_fixate (GstBaseSrc * src, GstCaps * caps);
-static gboolean gst_quicsrc_set_caps (GstBaseSrc * src, GstCaps * caps);
-static gboolean gst_quicsrc_decide_allocation (GstBaseSrc * src,
-    GstQuery * query);
+
 static gboolean gst_quicsrc_start (GstBaseSrc * src);
 static gboolean gst_quicsrc_stop (GstBaseSrc * src);
-static void gst_quicsrc_get_times (GstBaseSrc * src, GstBuffer * buffer,
-    GstClockTime * start, GstClockTime * end);
-static gboolean gst_quicsrc_get_size (GstBaseSrc * src, guint64 * size);
-static gboolean gst_quicsrc_is_seekable (GstBaseSrc * src);
-static gboolean gst_quicsrc_prepare_seek_segment (GstBaseSrc * src,
-    GstEvent * seek, GstSegment * segment);
-static gboolean gst_quicsrc_do_seek (GstBaseSrc * src, GstSegment * segment);
+
 static gboolean gst_quicsrc_unlock (GstBaseSrc * src);
 static gboolean gst_quicsrc_unlock_stop (GstBaseSrc * src);
 static gboolean gst_quicsrc_query (GstBaseSrc * src, GstQuery * query);
 static gboolean gst_quicsrc_event (GstBaseSrc * src, GstEvent * event);
-static GstFlowReturn gst_quicsrc_alloc (GstBaseSrc * src, guint64 offset,
-    guint size, GstBuffer ** buf);
-static GstFlowReturn gst_quicsrc_fill (GstBaseSrc * src, guint64 offset,
-    guint size, GstBuffer * buf);
 
 static GstFlowReturn gst_quicsrc_create (GstPushSrc * src, GstBuffer ** outbuf);
 
@@ -104,8 +93,6 @@ gst_quicsrc_class_init (GstQuicsrcClass * klass)
   GstBaseSrcClass *base_src_class = GST_BASE_SRC_CLASS (klass);
   GstPushSrcClass *push_src_class = (GstPushSrcClass *) klass;
 
-  /* Setting up pads and setting metadata should be moved to
-     base_class_init if you intend to subclass this class. */
   gst_element_class_add_static_pad_template (GST_ELEMENT_CLASS (klass),
       &gst_quicsrc_src_template);
 
@@ -118,25 +105,14 @@ gst_quicsrc_class_init (GstQuicsrcClass * klass)
   gobject_class->dispose = gst_quicsrc_dispose;
   gobject_class->finalize = gst_quicsrc_finalize;
   base_src_class->get_caps = GST_DEBUG_FUNCPTR (gst_quicsrc_get_caps);
-  base_src_class->negotiate = GST_DEBUG_FUNCPTR (gst_quicsrc_negotiate);
-  base_src_class->fixate = GST_DEBUG_FUNCPTR (gst_quicsrc_fixate);
-  base_src_class->set_caps = GST_DEBUG_FUNCPTR (gst_quicsrc_set_caps);
-  base_src_class->decide_allocation =
-      GST_DEBUG_FUNCPTR (gst_quicsrc_decide_allocation);
+
   base_src_class->start = GST_DEBUG_FUNCPTR (gst_quicsrc_start);
   base_src_class->stop = GST_DEBUG_FUNCPTR (gst_quicsrc_stop);
-  base_src_class->get_times = GST_DEBUG_FUNCPTR (gst_quicsrc_get_times);
-  base_src_class->get_size = GST_DEBUG_FUNCPTR (gst_quicsrc_get_size);
-  base_src_class->is_seekable = GST_DEBUG_FUNCPTR (gst_quicsrc_is_seekable);
-  base_src_class->prepare_seek_segment =
-      GST_DEBUG_FUNCPTR (gst_quicsrc_prepare_seek_segment);
-  base_src_class->do_seek = GST_DEBUG_FUNCPTR (gst_quicsrc_do_seek);
+
   base_src_class->unlock = GST_DEBUG_FUNCPTR (gst_quicsrc_unlock);
   base_src_class->unlock_stop = GST_DEBUG_FUNCPTR (gst_quicsrc_unlock_stop);
   base_src_class->query = GST_DEBUG_FUNCPTR (gst_quicsrc_query);
   base_src_class->event = GST_DEBUG_FUNCPTR (gst_quicsrc_event);
-  base_src_class->alloc = GST_DEBUG_FUNCPTR (gst_quicsrc_alloc);
-  base_src_class->fill = GST_DEBUG_FUNCPTR (gst_quicsrc_fill);
 
   push_src_class->create = GST_DEBUG_FUNCPTR (gst_quicsrc_create);
 }
@@ -200,59 +176,24 @@ gst_quicsrc_finalize (GObject * object)
   G_OBJECT_CLASS (gst_quicsrc_parent_class)->finalize (object);
 }
 
-/* get caps from subclass */
+/* Provides the basesrc with possible pad capabilities
+   to allow it to respond to queries from downstream elements.
+   Since the data transmitted over QUIC could be of any type, we 
+   return either ANY caps or, if provided, the caps of the
+   downstream element */
 static GstCaps *
 gst_quicsrc_get_caps (GstBaseSrc * src, GstCaps * filter)
 {
-  GstQuicsrc *quicsrc = GST_QUICSRC (src);
+  GstQuicsrc *quicsrc;
+  GstCaps *caps = NULL;
 
-  GST_DEBUG_OBJECT (quicsrc, "get_caps");
+  quicsrc = GST_QUICSRC (src);
 
-  return NULL;
-}
+  caps = (filter ? gst_caps_ref (filter) : gst_caps_new_any ());
 
-/* decide on caps */
-static gboolean
-gst_quicsrc_negotiate (GstBaseSrc * src)
-{
-  GstQuicsrc *quicsrc = GST_QUICSRC (src);
-
-  GST_DEBUG_OBJECT (quicsrc, "negotiate");
-
-  return TRUE;
-}
-
-/* called if, in negotiation, caps need fixating */
-static GstCaps *
-gst_quicsrc_fixate (GstBaseSrc * src, GstCaps * caps)
-{
-  GstQuicsrc *quicsrc = GST_QUICSRC (src);
-
-  GST_DEBUG_OBJECT (quicsrc, "fixate");
-
-  return NULL;
-}
-
-/* notify the subclass of new caps */
-static gboolean
-gst_quicsrc_set_caps (GstBaseSrc * src, GstCaps * caps)
-{
-  GstQuicsrc *quicsrc = GST_QUICSRC (src);
-
-  GST_DEBUG_OBJECT (quicsrc, "set_caps");
-
-  return TRUE;
-}
-
-/* setup allocation query */
-static gboolean
-gst_quicsrc_decide_allocation (GstBaseSrc * src, GstQuery * query)
-{
-  GstQuicsrc *quicsrc = GST_QUICSRC (src);
-
-  GST_DEBUG_OBJECT (quicsrc, "decide_allocation");
-
-  return TRUE;
+  GST_DEBUG_OBJECT (src, "returning caps %" GST_PTR_FORMAT, caps);
+  g_assert (GST_IS_CAPS (caps));
+  return caps;
 }
 
 /* start and stop processing, ideal for opening/closing the resource */
@@ -276,63 +217,28 @@ gst_quicsrc_stop (GstBaseSrc * src)
   return TRUE;
 }
 
-/* given a buffer, return start and stop time when it should be pushed
- * out. The base class will sync on the clock using these times. */
-static void
-gst_quicsrc_get_times (GstBaseSrc * src, GstBuffer * buffer,
-    GstClockTime * start, GstClockTime * end)
-{
-  GstQuicsrc *quicsrc = GST_QUICSRC (src);
+// /* given a buffer, return start and stop time when it should be pushed
+//  * out. The base class will sync on the clock using these times. */
+// static void
+// gst_quicsrc_get_times (GstBaseSrc * src, GstBuffer * buffer,
+//     GstClockTime * start, GstClockTime * end)
+// {
+//   GstQuicsrc *quicsrc = GST_QUICSRC (src);
 
-  GST_DEBUG_OBJECT (quicsrc, "get_times");
+//   GST_DEBUG_OBJECT (quicsrc, "get_times");
 
-}
+// }
 
-/* get the total size of the resource in bytes */
-static gboolean
-gst_quicsrc_get_size (GstBaseSrc * src, guint64 * size)
-{
-  GstQuicsrc *quicsrc = GST_QUICSRC (src);
+// /* get the total size of the resource in bytes */
+// static gboolean
+// gst_quicsrc_get_size (GstBaseSrc * src, guint64 * size)
+// {
+//   GstQuicsrc *quicsrc = GST_QUICSRC (src);
 
-  GST_DEBUG_OBJECT (quicsrc, "get_size");
+//   GST_DEBUG_OBJECT (quicsrc, "get_size");
 
-  return TRUE;
-}
-
-/* check if the resource is seekable */
-static gboolean
-gst_quicsrc_is_seekable (GstBaseSrc * src)
-{
-  GstQuicsrc *quicsrc = GST_QUICSRC (src);
-
-  GST_DEBUG_OBJECT (quicsrc, "is_seekable");
-
-  return TRUE;
-}
-
-/* Prepare the segment on which to perform do_seek(), converting to the
- * current basesrc format. */
-static gboolean
-gst_quicsrc_prepare_seek_segment (GstBaseSrc * src, GstEvent * seek,
-    GstSegment * segment)
-{
-  GstQuicsrc *quicsrc = GST_QUICSRC (src);
-
-  GST_DEBUG_OBJECT (quicsrc, "prepare_seek_segment");
-
-  return TRUE;
-}
-
-/* notify subclasses of a seek */
-static gboolean
-gst_quicsrc_do_seek (GstBaseSrc * src, GstSegment * segment)
-{
-  GstQuicsrc *quicsrc = GST_QUICSRC (src);
-
-  GST_DEBUG_OBJECT (quicsrc, "do_seek");
-
-  return TRUE;
-}
+//   return TRUE;
+// }
 
 /* unlock any pending access to the resource. subclasses should unlock
  * any function ASAP. */
@@ -395,30 +301,6 @@ gst_quicsrc_create (GstPushSrc * src, GstBuffer ** outbuf)
   lsquic_conn_crypto_cipher(NULL);
 
   GST_DEBUG_OBJECT (quicsrc, "create");
-
-  return GST_FLOW_OK;
-}
-
-/* ask the subclass to allocate an output buffer. The default implementation
- * will use the negotiated allocator. */
-static GstFlowReturn
-gst_quicsrc_alloc (GstBaseSrc * src, guint64 offset, guint size,
-    GstBuffer ** buf)
-{
-  GstQuicsrc *quicsrc = GST_QUICSRC (src);
-
-  GST_DEBUG_OBJECT (quicsrc, "alloc");
-
-  return GST_FLOW_OK;
-}
-
-/* ask the subclass to fill the buffer with data from offset and size */
-static GstFlowReturn
-gst_quicsrc_fill (GstBaseSrc * src, guint64 offset, guint size, GstBuffer * buf)
-{
-  GstQuicsrc *quicsrc = GST_QUICSRC (src);
-
-  GST_DEBUG_OBJECT (quicsrc, "fill");
 
   return GST_FLOW_OK;
 }
