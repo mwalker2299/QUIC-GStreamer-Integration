f3fd2b2c4f7792ad9d17e8478188a41e3b005602
Matthew <mjwalker2299@gmail.com>
2022-02-28 14:53:31 +0000

    Monitor client process when waiting for pipeline execution to end
    Previously, we would monitor the server gst-launch process and kill the other threads when this process finished or after a timeout has passed. However, it makes more sense to have the client inidicate end of execution, since the server side pipeline will always finish before the client. This commit swaps the controlling threads.
    
    This commit also replaces reference to tcpdump with "tshark" as the packet analyser we use is now tshark

--- src/mininet_tests/test_loop.py
+++ src/mininet_tests/test_loop.py
@@ -11,13 +11,13 @@ from mininet.util import dumpNetConnections
 import threading
 import network
 
-def streamServerThread(serverNode, cmd, timeout, event, log_path):
-  print("Stream Server: " + cmd)
+def streamControlThread(serverNode, cmd, timeout, event, log_path):
+  print("Stream Control: " + cmd)
   run_time = 0
 
   # To account for high loss scenarios where 
   # initial connection attempt fails to reach
-  # server, we run server in background.
+  # server, we run the controlling pipeine in background.
   # If the process has terminated within
   # The timeout, then we signal the other
   # threads to end their processes.
@@ -36,12 +36,12 @@ def streamServerThread(serverNode, cmd, timeout, event, log_path):
   print(cmd_done)
 
   if "No such process" in cmd_done:
-    print("Stream Server: process terminated within timeout.")
+    print("Stream Control: process terminated within timeout.")
     serverNode.cmd("touch " + os.path.join(log_path,"Success"))
   else:
     while (1):
 
-     print("Stream Server: Process still running, allowing more time")
+     print("Stream Control: Process still running, allowing more time")
      sleep(timeout/5)
      run_time += timeout/5
 
@@ -49,11 +49,11 @@ def streamServerThread(serverNode, cmd, timeout, event, log_path):
      print(cmd_done)
 
      if "No such process" in cmd_done:
-      print("Stream Server: process terminated after " + str(run_time) + " seconds.")
+      print("Stream Control: process terminated after " + str(run_time) + " seconds.")
       serverNode.cmd("touch " + os.path.join(log_path,"Success"))
       break
      elif run_time >= 3*timeout:
-       print("Stream Server: Process still running after " + str(run_time) + " seconds, killing process " + cmd_pid + " manually")
+       print("Stream Control: Process still running after " + str(run_time) + " seconds, killing process " + cmd_pid + " manually")
        serverNode.cmd('kill ' + cmd_pid)
        serverNode.cmd("touch " + os.path.join(log_path,"Failure"))
        break
@@ -63,17 +63,23 @@ def streamServerThread(serverNode, cmd, timeout, event, log_path):
   event.set()
   print("Stream Server: event set (Done)")
 
-def streamClientThread(clientNode, cmd, event):
-  print("Stream Client: starting client")
-  # Client should run in background to allow thread to kill process when signaled
+# This thread is used by the node which we wish to kill after the control node has completed.
+# For most implementations, we wish to wait for the client to finish execution so that we can 
+# get an accurate picture of the time taken to decode all frames. If we killed the client
+# when the server is done sending, we may lose some info. The only case where this is not applicable
+# is udp, since the client has no concept of connection and thus cant send an eos when the connection closes.
+# In the UDP case this is not an issue, as there will be no added delay due to packet loss.
+def streamWaitingThread(clientNode, cmd, event):
+  print("Stream Slave: starting client")
+  # Process should run in background to allow thread to kill process when signaled
   cmd = cmd + " &"
-  print("Stream Client: " + cmd)
+  print("Stream Slave: " + cmd)
   clientNode.cmd(cmd)
   cmd_pid = clientNode.cmd("echo $!")
-  print("Stream Client: Started Process in background, waiting for server completing")
+  print("Stream Slave: Started Process in background, waiting for control thread signal")
   event.wait()
 
-  print("Stream Client: event acquired, killing process " + cmd_pid)
+  print("Stream Slave: event acquired, killing process " + cmd_pid)
   clientNode.cmd('kill ' + cmd_pid)
 
 def crossTrafficClientThread(clientNode, cmd, event):
@@ -151,22 +157,22 @@ def run_test(test_params, stream_server_command, stream_client_command, ct_comma
   # UDP requires that stream_client and stream_server are given the stream_clients address, while QUIC is the opposite
   if "UDP" in protocol_name:
     print("PROTOCOL IS UDP")
-    client_side_tcpdump_command = "tshark -T fields -e frame.time -e rtp.seq -i s2-eth1 > " + os.path.join(log_path, "client_side_timestamps.txt")
-    server_side_tcpdump_command = "tshark -T fields -e frame.time -e rtp.seq -i s1-eth1 > " + os.path.join(log_path, "server_side_timestamps.txt")
+    client_side_tshark_command = "tshark -T fields -e frame.time -e rtp.seq -i s2-eth1 > " + os.path.join(log_path, "client_side_timestamps.txt")
+    server_side_tshark_command = "tshark -T fields -e frame.time -e rtp.seq -i s1-eth1 > " + os.path.join(log_path, "server_side_timestamps.txt")
 
     stream_server_command = stream_server_command.format(addr=stream_client.IP())
     stream_client_command = stream_client_command.format(addr=stream_client.IP(), buffer_delay=test_params["buffer_delay"])
   elif "QUIC" in protocol_name:
     print("PROTOCOL IS QUIC")
-    client_side_tcpdump_command = "tshark -d udp.port==5000,quic -T fields -e frame.time -e quic.stream.stream_id -e quic.stream.length -e quic.stream.fin -e quic.stream_data -o \"tls.keylog_file:"+os.path.join(log_path, "SSL.keys")+"\" -i s2-eth1 > " + os.path.join(log_path, "client_side_timestamps.txt")
-    server_side_tcpdump_command = "tshark -d udp.port==5000,quic -T fields -e frame.time -e quic.stream.stream_id -e quic.stream.length -e quic.stream.fin -e quic.stream_data -o \"tls.keylog_file:"+os.path.join(log_path, "SSL.keys")+"\" -i s1-eth1 > " + os.path.join(log_path, "server_side_timestamps.txt")
+    client_side_tshark_command = "tshark -d udp.port==5000,quic -T fields -e frame.time -e quic.stream.stream_id -e quic.stream.offset -e quic.stream.fin -e quic.stream_data -o \"tls.keylog_file:"+os.path.join(log_path, "SSL.keys")+"\" -i s2-eth1 > " + os.path.join(log_path, "client_side_timestamps.txt")
+    server_side_tshark_command = "tshark -d udp.port==5000,quic -T fields -e frame.time -e quic.stream.stream_id -e quic.stream.offset -e quic.stream.fin -e quic.stream_data -o \"tls.keylog_file:"+os.path.join(log_path, "SSL.keys")+"\" -i s1-eth1 > " + os.path.join(log_path, "server_side_timestamps.txt")
 
     stream_server_command = stream_server_command.format(addr=stream_server.IP(), lsquic_log=server_lsquic_log_path, keylog=server_keylog_path)
     stream_client_command = stream_client_command.format(addr=stream_server.IP(), lsquic_log=client_lsquic_log_path, buffer_delay=test_params["buffer_delay"])
   else:
     print("PROTOCOL IS TCP")
-    client_side_tcpdump_command = "tshark -d tcp.port==5000,rtp -T fields -e frame.time -e tcp.nxtseq -e tcp.payload -i s2-eth1 -o tcp.reassemble_out_of_order:TRUE > " + os.path.join(log_path, "client_side_timestamps.txt")
-    server_side_tcpdump_command = "tshark -d tcp.port==5000,rtp -T fields -e frame.time -e tcp.nxtseq -e rtp.seq -i s1-eth1 -o tcp.reassemble_out_of_order:TRUE > " + os.path.join(log_path, "server_side_timestamps.txt")
+    client_side_tshark_command = "tshark -d tcp.port==5000,rtp -T fields -e frame.time -e tcp.nxtseq -e tcp.payload -i s2-eth1 -o tcp.reassemble_out_of_order:TRUE > " + os.path.join(log_path, "client_side_timestamps.txt")
+    server_side_tshark_command = "tshark -d tcp.port==5000,rtp -T fields -e frame.time -e tcp.nxtseq -e rtp.seq -i s1-eth1 -o tcp.reassemble_out_of_order:TRUE > " + os.path.join(log_path, "server_side_timestamps.txt")
 
     stream_server_command = stream_server_command.format(addr=stream_server.IP(), lsquic_log=server_lsquic_log_path, keylog=server_keylog_path)
     stream_client_command = stream_client_command.format(addr=stream_server.IP(), lsquic_log=client_lsquic_log_path, buffer_delay=test_params["buffer_delay"])
@@ -186,18 +192,22 @@ def run_test(test_params, stream_server_command, stream_client_command, ct_comma
   # the stream_server completes its transmission
   event = threading.Event()
 
-  # Create threads for stream_server node, stream_client node and client-side switch node (for tcpdump)
-  stream_server_thread  = threading.Thread(target=streamServerThread, args=(stream_server,stream_server_command,timeout,event,log_path))
-  stream_client_thread  = threading.Thread(target=streamClientThread, args=(stream_client,stream_client_command,event))
+  # Create threads for stream_server node, stream_client node and client-side switch node (for tshark)
+  if "UDP" in protocol_name:
+    stream_server_thread  = threading.Thread(target=streamControlThread, args=(stream_server,stream_server_command,timeout,event,log_path))
+    stream_client_thread  = threading.Thread(target=streamWaitingThread, args=(stream_client,stream_client_command,event))
+  else:
+    stream_server_thread  = threading.Thread(target=streamWaitingThread, args=(stream_server,stream_server_command,event))
+    stream_client_thread  = threading.Thread(target=streamControlThread, args=(stream_client,stream_client_command,timeout,event,log_path))
 
   if cross_traffic_enabled:
     ct_client_thread =  threading.Thread(target=crossTrafficClientThread, args=(cross_traffic_client,ct_command,event))
 
-  client_side_monitor_thread = threading.Thread(target=monitorThread, args=(client_side_switch,client_side_tcpdump_command,event))
-  server_side_monitor_thread = threading.Thread(target=monitorThread, args=(server_side_switch,server_side_tcpdump_command,event))
+  client_side_monitor_thread = threading.Thread(target=monitorThread, args=(client_side_switch,client_side_tshark_command,event))
+  server_side_monitor_thread = threading.Thread(target=monitorThread, args=(server_side_switch,server_side_tshark_command,event))
 
   # Start threads, wait for thread completion and then stop the network
-  # We start the monitor threads first and sleep for 1 second so that tcpdump
+  # We start the monitor threads first and sleep for 1 second so that tshark
   # Does not miss any packets
   client_side_monitor_thread.start()
   server_side_monitor_thread.start()
