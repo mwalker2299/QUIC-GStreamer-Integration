9d2f7662882a17b206d1213fc228eea35712cca3
Matthew <mjwalker2299@gmail.com>
2022-02-28 14:48:04 +0000

    keep_test_running.sh should append rather than overwrite
    keep_test_running.sh restarts the tests in case of the process crashing. Currently, the output is written to a stdout and stderr file. If a restart occurs, the output for the initial part of the run will be lost.
    
    This commit changes the redirect such that we append to the output file, rather than overwriting.

--- src/mininet_tests/keep_test_running.sh
+++ src/mininet_tests/keep_test_running.sh
@@ -4,5 +4,5 @@ while [ ! -f /home/matt/T7/mininet_results/DONE ] ;
 do
       killall gst-launch-1.0
       mn -c
-      python3 test_runner.py -i 1 -o /home/matt/T7/ >test_results_stdout.txt 2>test_results_stderr.txt
+      python3 test_runner.py -i 5 -o /home/matt/T7/ >>test_results_stdout.txt 2>>test_results_stderr.txt
 done
