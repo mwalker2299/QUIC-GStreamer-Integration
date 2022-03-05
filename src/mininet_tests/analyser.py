4d54049ee9b0bdf9975e5165ab0f3a54acfc2c97
Matthew <mjwalker2299@gmail.com>
2022-03-05 22:29:37 +0000

    Utilise frame latency analysis in analyser

--- src/mininet_tests/analyser.py
+++ src/mininet_tests/analyser.py
@@ -38,14 +38,25 @@ def analyse(results_path):
             stack_latency_pd = stack_latency_analysis.convert_results_to_panda(stack_latency)
             stack_latency_analysis.save_results_datagram(time_diff_panda=stack_latency_pd, directory=dirpath)
 
-          # Extract app latency data
-          app_latency    = gstreamer_log_analysis.extract_Nal_unit_data(dirpath)
-          frame_stats_pd = gstreamer_log_analysis.identify_useful_units(app_latency)
-          app_latency_pd = gstreamer_log_analysis.convert_nal_results_to_panda(app_latency)
-          gstreamer_log_analysis.save_nal_results(time_diff_panda=app_latency_pd, frame_stats_panda=frame_stats_pd, directory=dirpath)
+          # Extract nal latency data
+          nal_latency    = gstreamer_log_analysis.extract_Nal_unit_data(dirpath)
+          nal_latency_pd = gstreamer_log_analysis.convert_nal_results_to_panda(nal_latency)
+          gstreamer_log_analysis.save_nal_results(time_diff_panda=nal_latency_pd, directory=dirpath)
+
+          # Extract frame latency data
+          frame_latency    = gstreamer_log_analysis.extract_frame_data(dirpath)
+          frame_latency_pd = gstreamer_log_analysis.convert_frame_results_to_panda(frame_latency)
+          frame_stats_pd   = gstreamer_log_analysis.identify_useful_frames(frame_latency)
+          gstreamer_log_analysis.save_frame_results(time_diff_panda=frame_latency_pd, frame_stats_panda=frame_stats_pd, directory=dirpath)
+
+
         except Exception as e:
           tb = traceback.format_exc()
           print("exception while processing" + dirpath)
           print("exception is " + str(e))
           print("traceback is: ", tb)
           print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
+
+# analyse("/home/matt/T7/mininet_results/UDP/")
+analyse("/home/matt/T7/mininet_results/QUIC_PPS/Delay_10ms/Loss_0%/Jitter_0ms/Buffer_Delay_42ms")
